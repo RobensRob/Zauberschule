1cc8f1f17a45e0f093422b3956d2f011ae003873
Robin <76265253+RobensRob@users.noreply.github.com>
2023-09-08 23:26:40 +0200

    Updated from 2d Array to 3d Array

--- src/main/java/de/robsrob/Main.java
+++ src/main/java/de/robsrob/Main.java
@@ -3,45 +3,44 @@ package de.robsrob;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws Exception{
         File file = new File("D:/Coding/Programs/Zauberschule/src/main/java/de/robsrob/aufgaben/zauberschule0.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
 
-        char[][] labyrinth1;
-        char[][] labyrinth2;
+        char[][][] lab;
 
         String[] yx = br.readLine().split(" ");
         int y = Integer.parseInt(yx[0]);
         int x = Integer.parseInt(yx[1]);
 
-        labyrinth1 = new char[y][x];
-        labyrinth2 = new char[y][x];
+        lab = new char[2][y][x];
 
-        fillMatrix(br, labyrinth1, y, x);
-        br.readLine();
-        fillMatrix(br, labyrinth2, y, x);
-
-
-
+        fill3dMatrix(br, lab, y, x);
+        print3dMatrix(lab, 0);
+        System.out.println();
+        print3dMatrix(lab, 1);
     }
 
-    static void fillMatrix(BufferedReader br, char[][] matrix, int y, int x) throws Exception{
-        for(int i = 0; i < y; i++) {
-            char[] currentLine = br.readLine().toCharArray();
-            for(int j = 0; j < x; j++) {
-                matrix[i][j] = currentLine[j];
+    static void fill3dMatrix(BufferedReader br, char[][][] matrix, int y, int x) throws Exception {
+        for(int i = 0; i <= 1; i++) {
+            for(int j = 0; j < y; j++) {
+                String line = br.readLine();
+                if(!line.equals("")) {
+                    char[] currentLine = line.toCharArray();
+                    for(int k = 0; k < x; k++) {
+                        matrix[i][j][k] = currentLine[k];
+                    }
+                }
             }
         }
     }
 
-    static void printMatrix(char matrix[][]) {
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + "\t");
+    static void print3dMatrix(char matrix[][][], int floor) {
+        for(int i = 0; i < matrix[0].length; i++) {
+            for(int j = 0; j < matrix[0][i].length; j++) {
+                System.out.print(matrix[0][i][j] + "\t");
             }
             System.out.println();
         }
