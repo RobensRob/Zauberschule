48c41b735136a61fca2ca9d382a9ea142d775fb9
Robin <76265253+RobensRob@users.noreply.github.com>
2023-09-11 23:25:12 +0200

    Made matrix-filling work perfectly

--- src/main/java/de/robsrob/Main.java
+++ src/main/java/de/robsrob/Main.java
@@ -18,9 +18,6 @@ public class Main {
         lab = new char[2][y][x];
 
         fill3dMatrix(br, lab, y, x);
-        print3dMatrix(lab, 0);
-        System.out.println();
-        print3dMatrix(lab, 1);
     }
 
     static void fill3dMatrix(BufferedReader br, char[][][] matrix, int y, int x) throws Exception {
@@ -28,19 +25,22 @@ public class Main {
             for(int j = 0; j < y; j++) {
                 String line = br.readLine();
                 if(!line.equals("")) {
+                    System.out.println(line);
                     char[] currentLine = line.toCharArray();
                     for(int k = 0; k < x; k++) {
                         matrix[i][j][k] = currentLine[k];
                     }
+                } else {
+                    j--;
                 }
             }
         }
     }
 
     static void print3dMatrix(char matrix[][][], int floor) {
-        for(int i = 0; i < matrix[0].length; i++) {
-            for(int j = 0; j < matrix[0][i].length; j++) {
-                System.out.print(matrix[0][i][j] + "\t");
+        for(int i = 0; i < matrix[floor].length; i++) {
+            for(int j = 0; j < matrix[floor][i].length; j++) {
+                System.out.print(matrix[floor][i][j] + "\t");
             }
             System.out.println();
         }
